016985c71ef3b38e62e631a84aa0f0dbaeae7ff2
Jim Mar <majinjing3@gmail.com>
2016-05-30 00:13:50 -0400

    update certbot and change renew command

--- ssl/renew_all_certs.sh
+++ ssl/renew_all_certs.sh
@@ -7,6 +7,9 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo "renew for domains $DOMAINS"
 echo "store challege in $SERVE_DIR"
 
-mkdir -p $SERVE_DIR && $DIR/../letsencrypt/letsencrypt-auto --renew certonly --server $SERVER -a webroot --webroot-path=$SERVE_DIR --agree-tos $DOMAINS
+#mkdir -p $SERVE_DIR && $DIR/../letsencrypt/letsencrypt-auto --renew certonly --server $SERVER -a webroot --webroot-path=$SERVE_DIR --agree-tos $DOMAINS
+
+# when we apply certificates, just renew is OK
+mkdir -p $SERVE_DIR && $DIR/../letsencrypt/letsencrypt-auto renew
 
 /usr/sbin/nginx -s reload
