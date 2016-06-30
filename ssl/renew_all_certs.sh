6fcbb7cd370606d43854c4f316f2b280bbc6bbd6
songchenwen <me@songchenwen.com>
2016-06-30 15:18:10 +0800

    New Parameter

--- ssl/renew_all_certs.sh
+++ ssl/renew_all_certs.sh
@@ -7,6 +7,6 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo "renew for domains $DOMAINS"
 echo "store challege in $SERVE_DIR"
 
-mkdir -p $SERVE_DIR && $DIR/../letsencrypt/letsencrypt-auto --renew certonly --server $SERVER -a webroot --webroot-path=$SERVE_DIR --agree-tos $DOMAINS
+mkdir -p $SERVE_DIR && $DIR/../letsencrypt/letsencrypt-auto --renew-by-default certonly --server $SERVER -a webroot --webroot-path=$SERVE_DIR --agree-tos $DOMAINS
 
 /usr/sbin/nginx -s reload
